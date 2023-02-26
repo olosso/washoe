9c98bbed2d4f831c470595013ddfdcc63c38d128
olosso <sorsa.olli@gmail.com>
2023-02-27 01:02:17 +0200

    repl: hooked to compiler now

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,8 +1,10 @@
 use crate::ast::*;
+use crate::compiler::*;
 use crate::evaluator::*;
 use crate::lexer::*;
 use crate::object::*;
 use crate::parser::*;
+use crate::vm::*;
 use std::io::{self, Write};
 use std::rc::Rc;
 
@@ -12,33 +14,30 @@ impl REPL {
     pub fn run() -> io::Result<()> {
         println!("Welcome to the REPL.");
         println!("Enter 'q' to quit the intrepreter.");
-        let environment = Rc::new(Environment::global());
+
         loop {
             print!(">>> ");
             io::stdout().flush().unwrap();
             let mut buffer = String::new();
             io::stdin().read_line(&mut buffer)?;
+
             if buffer.as_str().trim() == "q" {
                 break;
             }
-            if buffer.as_str().trim() == "env" || buffer.as_str().trim() == "" {
-                println!("{}", environment.inspect());
-                continue;
-            }
+
             let mut lexer = Lexer::new(buffer);
             let mut parser = Parser::new(lexer);
+
             match parser.parse_program() {
-                Ok(program) => match eval(&program, &environment) {
-                    Ok(value) => println!("{}", value.inspect()),
-                    Err(e) => {
-                        println!("You got an Evaluation error: {e}\nBut that's okay 😸");
-                        continue;
-                    }
-                },
-                Err(e) => {
-                    println!("You got an Parsing error: {e}\nBut that's okay 😻");
-                    continue;
+                Ok(program) => {
+                    let mut compiler = Compiler::default();
+                    compiler.compile(program);
+                    let mut vm = VM::new(compiler.bytecode());
+                    vm.run();
+                    let top = vm.stack_top().unwrap();
+                    println!("{top:?}",);
                 }
+                Err(err) => println!("Eeeek, failed to parse program."),
             }
         }
 
